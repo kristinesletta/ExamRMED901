4bd89d28ca5314956f84bdd2f0b44e6f762e98e3
kristinesletta <kristine.sletta@uib.no>
2021-10-07 13:57:53 +0200

    adding more comments in the script adding plots

--- Scripts/Wrangling_script_exam.R
+++ Scripts/Wrangling_script_exam.R
@@ -1,11 +1,11 @@
 Trying things for the codebook_exam dataset
 
 
-Trying the delimeter separating data fields, but found out that it wasnt necessary for this dataset as the delimimiter is already set as tab
+1. Trying the delimeter separating data fields, but found out that it wasnt necessary for this dataset as the delimimiter is already set as tab
 Exam_data <- read_delim(here("Data", "exam_data.txt"), delim = "\t")
 Exam_data
 
-tried to separate the first column with following code:
+2. tried to separate the first column with following code:
   
   Here I try to separate the first column into two subcaterogies:
   ```{r}
@@ -15,3 +15,14 @@ covid_data <-
            into = c ("PatientID", "Patient_Name"), separate = "-")
 
 got an error. Needed to add the merge function to add values together
+
+3. tried to cahnge the columns age_gender but suddely got 31048 observations wiht this function:
+  clean_covid<-
+  clean_covid %>% 
+  pivot_longer(names_to = "gender", values_to = "age", cols=15:16)
+clean_covid
+
+we created a duplicate number because of the "NA"
+then added another filter funciton to remove the NA´s in the age column
+
+4. we tried different geom_plots to vizualize and analyse the waiting time over the 100 days. 
